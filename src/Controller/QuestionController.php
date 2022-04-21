f4a364b54afe330de30233f06af7dd76316070ad
AzizaJonathan <azizajonathan74@gmail.com>
2022-04-21 16:01:55 +0200

    Viewing question details

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -38,20 +38,8 @@ class QuestionController extends AbstractController
   }
 
   #[Route('/{id}', name: 'show')]
-  public function show(Request $request, string $id): Response
+  public function show(Question $question): Response
   {
-
-    $question =   [
-      'title' => 'Je suis une super question',
-      'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-      'rating' => 20,
-      'author' => [
-        'name' => 'Jean Dupont',
-        'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
-      ],
-      'nbrOfResponse' => 15
-    ];
-
     return $this->render('question/show.html.twig', [
       'question' => $question,
     ]);
