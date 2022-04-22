7ce8dba7929bbbf63055c14d6bed61dfa019d53a
AzizaJonathan <azizajonathan74@gmail.com>
2022-04-22 13:51:56 +0200

    Creating a User Controller

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -221,4 +221,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullname(): ?string
+    {
+    return $this->firstname . ' ' . $this->lastname;
+    }
 }
